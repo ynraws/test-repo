d6c0b0e9f0cfc2d2b47b36efffa40a29eb0d3d8f
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-03-23 23:22:03 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -7,13 +7,13 @@ variable "project" {
 variable "region" {
   description = "this is gcp region"
   type        = string
-  default     = "asia-south1"
+  default     = "us-east1"
 }
 
 variable "zone" {
   description = "this is gcp zone"
   type        = string
-  default     = "asia-south1-a"
+  default     = "us-east1-a"
 }
 
 variable "K8s_version" {
