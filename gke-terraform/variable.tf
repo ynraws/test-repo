a062672618947f0e6127670d62450c973b3950c0
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-03-25 14:53:40 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,7 +1,7 @@
 variable "project" {
   description = "this is gcp project-id"
   type        = string
-  default     = "rock-loop-449918-b0"
+  default     = "argon-zoo-454615-t9"
 }
 
 variable "region" {
