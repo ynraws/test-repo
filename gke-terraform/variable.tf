8a6960cf015345d0070b85dc95a36b0035fe9d23
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-03-25 14:53:09 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,1 +1,29 @@
+variable "project" {
+  description = "this is gcp project-id"
+  type        = string
+  default     = "rock-loop-449918-b0"
+}
 
+variable "region" {
+  description = "this is gcp region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "this is gcp zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "K8s_version" {
+  description = "this is the gke version"
+  type        = string
+  default     = "1.31.6-gke.1020000"
+}
+
+variable "node-count" {
+  description = "this is the gke node count"
+  type        = number
+  default     = 2
+}
