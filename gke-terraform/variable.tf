444f96daf82fb1350b309bc14029b001678255e6
ynraws <162948247+ynraws@users.noreply.github.com>
2025-07-24 12:17:12 +0530

    Update variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,1 +1,23 @@
+variable "project" {
+  description = "this is gcp project-id"
+  type        = string
+  default     = "rock-loop-449918-b0"
+}
 
+variable "region" {
+  description = "this is gcp region"
+  type        = string
+  default     = "africa-south1"
+}
+
+variable "zone" {
+  description = "this is gcp zone"
+  type        = string
+  default     = "africa-south1-a"
+}
+
+variable "K8s_version" {
+  description = "this is the gke version"
+  type        = string
+  default     = "1.31.6-gke.1020000"
+}
